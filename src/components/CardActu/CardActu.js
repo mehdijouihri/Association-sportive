15c7900506ab2086246d0b8ab13918a0bf5f9147
arnaudonline <arnaud.beaugeon@live.fr>
2022-05-23 11:10:52 +0200

    [+] CardActu component is OK

---
+++ src/components/CardActu/CardActu.js
@@ -0,0 +1,16 @@
+import './CardActu.css';
+
+const CardActu = (props) => {
+    return (
+        <article className="card_actu">
+            <img src={props.img} alt="" />
+            <div className="actu_details">
+                <p className="actu_category">{props.category}</p>
+                <p className="actu_date">{props.date}</p>
+            </div>
+            <h2>{props.title}</h2>
+        </article>
+    );
+};
+
+export default CardActu;
