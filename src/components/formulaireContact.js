c99cc0b3d56a51fe3cef51eaa218687c5fbf3359
LoicDerouard <loicderouard@hotmail.fr>
2022-05-24 16:19:34 +0200

    envoie responsive formulaire de contact

--- src/components/formulaireContact.js
+++ src/components/formulaireContact.js
@@ -5,43 +5,47 @@ const Contact = () => {
     return (
 
         <>
-            <div class="contactez-nous">
-                <h1>Vous souhaitez prendre contact avec nous ?</h1>
-                <p>Merci de remplir le formulaire ci-dessous :</p>
-                <form action="/page-traitement-donnees" method="post">
-                    <div className='partien'>
-                        <div className='formule'>
-                            <label for="nom"></label>
-                            <input type="text" id="nom" name="nom" placeholder="Nom" required></input>
+            <div className='COMPLETCONTACT'>
+                <img src={require('../assets/Images/image-contact.png')}></img>
+                <div class="contactez-nous">
+                    <h1>Vous souhaitez prendre contact avec nous ?</h1>
+                    <p>Merci de remplir le formulaire ci-dessous&nbsp;:</p>
+                    <form action="/page-traitement-donnees" method="post">
+                        <div className='partien'>
+                            <div className='formule'>
+                                <label for="nom"></label>
+                                <input type="text" id="nom" name="nom" placeholder="Nom" required></input>
+                            </div>
+                            <div className='formule'>
+                                <label for="prenom"></label>
+                                <input type="text" id="nom" name="nom" placeholder="Prénom" required></input>
+                            </div>
                         </div>
-                        <div className='formule'>
-                            <label for="prenom"></label>
-                            <input type="text" id="nom" name="nom" placeholder="Prénom*" required></input>
+                        <div className='partiea'>
+                            <div>
+                                <label for="numtel"></label>
+                                <input type="text" id="nom" name="nom" placeholder="Numéro de tel" required></input>
+                            </div>
+                            <div className='formule'>
+                                <label for="email"></label>
+                                <input type="email" id="email" name="email" placeholder="Adresse mail" required></input>
+                            </div>
                         </div>
-                    </div>
-                    <div className='partiea'>
-                        <div>
-                            <label for="numtel"></label>
-                            <input type="text" id="nom" name="nom" placeholder="Numéro de tel*" required></input>
-                        </div>
-                        <div className='formule'>
-                            <label for="email"></label>
-                            <input type="email" id="email" name="email" placeholder="Adresse mail*" required></input>
-                        </div>
-                    </div>
 
-                    <div className='formule'>
-                        <label for="message"></label>
-                        <textarea id="message" name="message" placeholder="Indiquez votre message ici..." required></textarea>
-                    </div>
-                    <div className='vendame'>
-                        <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter"></input>
+                        <div className='formule'>
+                            <label for="message"></label>
+                            <textarea id="message" name="message" placeholder="Indiquez votre message ici..." required></textarea>
+                        </div>
+                        <div className='vendame'>
+                            <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter" required></input>
                             <label for="subscribeNews">J'accepte la politique de confidentialité</label>
-                    </div>
-                    <div className='formule'>
-                        <button className='boutoncontact' type="submit">Envoyer mon message</button>
-                    </div>
-                </form>
+                        </div>
+                        <div className='formule'>
+                            <button className='boutoncontact' type="submit">Envoyer un message</button>
+                        </div>
+
+                    </form>
+                </div>
             </div>
         </>
     );
