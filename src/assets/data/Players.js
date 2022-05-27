bc022417615446ef42d3f88acac69a24498f8118
arnaudonline <arnaud.beaugeon@live.fr>
2022-05-27 09:45:25 +0200

    [+] Add Players JSON

---
+++ src/assets/data/Players.js
@@ -0,0 +1,62 @@
+export const PLAYERS = [
+  {
+    "img": require('../images/player01.jpg'),
+    "name": "Jean Patry",
+    "role": "arriere droit",
+    "birth": "1996-12-27",
+    "taille": 207,
+    "poids": 94,
+    "HDattaque": 334,
+    "HBlock": 317
+  },
+  {
+    "img": require('../images/player01.jpg'),
+    "name": "Jeannette Kappi",
+    "role": "avant droit",
+    "birth": "1997-08-20",
+    "taille": 190,
+    "poids": 86,
+    "HDattaque": 314,
+    "HBlock": 302
+  },
+  {
+    "img": require('../images/player01.jpg'),
+    "name": "Jenia Grebennikov",
+    "role": "avant gauche",
+    "birth": "1994-01-25",
+    "taille": 201,
+    "poids": 95,
+    "HDattaque": 328,
+    "HBlock": 313
+  },
+  {
+    "img": "",
+    "name": "Barthélémy Chinenyeze",
+    "role": "arrière gauche",
+    "birth": "1996-12-27",
+    "taille": 201,
+    "poids": 101,
+    "HDattaque": 340,
+    "HBlock": 311
+  },
+  {
+    "img": "",
+    "name": "Nicolas Le Goff",
+    "role": "arrière centre",
+    "birth": "1996-04-10",
+    "taille": 205,
+    "poids": 90,
+    "HDattaque": 305,
+    "HBlock": 303
+  },
+  {
+    "img": "",
+    "name": "Daryl Bultor",
+    "role": "arriere droit",
+    "birth": "1993-01-25",
+    "taille": 197,
+    "poids": 100,
+    "HDattaque": 323,
+    "HBlock": 310
+  }
+]
