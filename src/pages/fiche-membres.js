d5515b2f31f7dd138fc7398125b752d4be0795e7
arnaudonline <arnaud.beaugeon@live.fr>
2022-06-06 13:11:34 +0200

    [~] Homepage is OK

--- src/pages/fiche-membres.js
+++ src/pages/fiche-membres.js
@@ -3,14 +3,12 @@ import Navbar from '../components/Header/Navbar';
 import "../components/FicheMembres/FicheMembres.css";
 import FicheMembres from '../components/FicheMembres/FicheMembres';
 import Footer from '../components/Footer/Footer';
-import Carousel from '../components/Carousel/Carousel';
 
 const PficheMembres = () => {
     return (
         <div>
             <Navbar/>
             <FicheMembres/>
-            <Carousel/>
             <Footer/>
         </div>
     );
