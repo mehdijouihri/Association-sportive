d5bdef12fb1b10d7b6cedfa829b873baafa66b1e
arnaudonline <arnaud.beaugeon@live.fr>
2022-06-08 07:39:28 +0200

    [~] Update CardActu

--- src/components/CardActu/CardActu.js
+++ src/components/CardActu/CardActu.js
@@ -3,12 +3,14 @@ import React from 'react';
 const CardActu = (props) => {
     return (
         <article className="card_actu" key={props.key}>
+            <a href={props.id}>
             <img src={props.img} alt="" />
             <div className="actu_details">
                 <p className="actu_category">{props.category}</p>
                 <p className="actu_date">{props.date}</p>
             </div>
             <h2>{props.title}</h2>
+            </a>
         </article>
     );
 };
